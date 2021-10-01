dacefd3727816feaa9da0bef01d1790a965080b1
Vadim Hladchenko <vadimhl@gmail.com>
2021-10-01 13:39:31 +0000

    post test

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = require('../app');
 const User = require('../models/user');
 const api = supertest(app);
+const helper = require('./test_helper');
 
 describe( 'user', () => {
     beforeEach( async () => {
@@ -17,11 +18,27 @@ describe( 'user', () => {
         await api.get('/api/users')
         .expect(200)
         .expect('Content-Type', /application\/json/);
-      })
-      test ( 'count', async () => {
+    })
+    test ( 'count', async () => {
         const response = await api.get('/api/users/'); 
         expect( response.body.length).toBe( 1 )
-      })
+    })
+    test ('post', async () => {
+        userToAdd = { username: 'posted', name: 'Posted User'};
+        const newUser = await api
+        .post('/api/users')
+        .send(userToAdd)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+        
+        expect(newUser.body.username).toBe('posted');
+        expect(newUser.body.name).toBe('Posted User');
+
+        const allUsers = await helper.usersInDb();
+        expect( allUsers.map( u => u.username)).toContain('posted');
+        expect( allUsers.map( u => u.name)).toContain('Posted User');
+
+    })
 })
 
 afterAll(() => {
