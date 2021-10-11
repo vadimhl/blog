c44afeafcdcd45b1b937a4899315b1d22b4f43b1
Vadim Hladchenko <vadimhl@gmail.com>
2021-10-11 13:15:35 +0000

    front set up

--- frontend/src/services/blogs.js
+++ frontend/src/services/blogs.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
+const baseUrl = 'https://3001-copper-peacock-recgkxr0.ws-eu18.gitpod.io/api/blogs'
 
+axios.defaults.withCredentials = true;
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
-
-export default { getAll }
+const blogService = { getAll }
+export default blogService
